6b32c69883e019d4c9235c8397d4100a3ee327ab
aniknaemmm <aniknaemmm@gmail.com>
2016-02-24 19:50:37 +0300

    example add new feature

--- WPFApplication/MainWindow.xaml.cs
+++ WPFApplication/MainWindow.xaml.cs
@@ -1,105 +1,34 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows;
+using WPFApplication;
 
-namespace FirstWpfApp
+namespace XamlApp
 {
     public partial class MainWindow : Window
     {
-        string leftop = ""; // Левый операнд
-        string operation = ""; // Знак операции
-        string rightop = ""; // Правый операнд
+        TlSharpCl t = new TlSharpCl();
+
 
         public MainWindow()
         {
             InitializeComponent();
-            // Добавляем обработчик для всех кнопок на гриде
-            foreach (UIElement c in LayoutRoot.Children)
-            {
-                if (c is Button)
-                {
-                    ((Button)c).Click += Button_Click;
-                }
-            }
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            // Получаем текст кнопки
-            string s = (string)((Button)e.OriginalSource).Content;
-            // Добавляем его в текстовое поле
-            textBlock.Text += s;
-            int num;
-            // Пытаемся преобразовать его в число
-            bool result = Int32.TryParse(s, out num);
-            // Если текст - это число
-            if (result == true)
-            {
-                // Если операция не задана
-                if (operation == "")
-                {
-                    // Добавляем к левому операнду
-                    leftop += s;
-                }
-                else
-                {
-                    // Иначе к правому операнду
-                    rightop += s;
-                }
-            }
-            // Если было введено не число
-            else
-            {
-                // Если равно, то выводим результат операции
-                if (s == "=")
-                {
-                    Update_RightOp();
-                    textBlock.Text += rightop;
-                    operation = "";
-                }
-                // Очищаем поле и переменные
-                else if (s == "CLEAR")
-                {
-                    leftop = "";
-                    rightop = "";
-                    operation = "";
-                    textBlock.Text = "";
-                }
-                // Получаем операцию
-                else
-                {
-                    // Если правый операнд уже имеется, то присваиваем его значение левому
-                    // операнду, а правый операнд очищаем
-                    if (rightop != "")
-                    {
-                        Update_RightOp();
-                        leftop = rightop;
-                        rightop = "";
-                    }
-                    operation = s;
-                }
-            }
+            t.AuthUser();
         }
-        // Обновляем значение правого операнда
-        private void Update_RightOp()
+
+
+        private async void Button_Click2(object sender, RoutedEventArgs e)
         {
-            int num1 = Int32.Parse(leftop);
-            int num2 = Int32.Parse(rightop);
-            // И выполняем операцию
-            switch (operation)
+
+            string text = textBox1.Text;
+            if (text != "")
             {
-                case "+":
-                    rightop = (num1 + num2).ToString();
-                    break;
-                case "-":
-                    rightop = (num1 - num2).ToString();
-                    break;
-                case "*":
-                    rightop = (num1 * num2).ToString();
-                    break;
-                case "/":
-                    rightop = (num1 / num2).ToString();
-                    break;
+                await t.EnterCode(text);
             }
         }
+
+
     }
 }
---
+++ WPFApplication/TlSharpCl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TLSharp.Core;
+
+namespace WPFApplication
+{
+    public class  TlSharpCl
+    {
+        public FileSessionStore Store ;
+        public TelegramClient Client;
+        public string Hash;
+        public TlSharpCl()
+        {
+             Store = new FileSessionStore();
+             Client = new TelegramClient(Store, "session");
+        }
+
+        public async Task AuthUser()
+        {
+            await Client.Connect();
+            Hash = await Client.SendCodeRequest("375257307554");
+    
+        }
+
+
+        public async Task EnterCode(string codes)
+        {            
+            var code = codes;
+            var user = await Client.MakeAuth("375257307554", Hash, code);
+               
+        }
+
+        public async Task sendMessage(string number,string message)
+        {
+            var userByPhoneId = await Client.ImportContactByPhoneNumber(number);
+            await Client.SendMessage(userByPhoneId.Value, message);
+        }
+
+    }
+}
